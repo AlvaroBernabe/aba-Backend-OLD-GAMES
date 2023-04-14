63335f4ec3d3fb2a4e757615559f22e4ae3f83f8
alvarito101093 <alvaro101093@gmail.com>
2023-04-14 15:21:52 +0200

    feat: Middleware isAdmin added

---
+++ Final-Proyect-Backend/app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+        public function handle(Request $request, Closure $next)
+        {
+            Log::info("Middleware");
+            $userId = auth()->user()->id;
+            $user = User::find($userId);
+            $role = $user->role_id;
+            if ($role != 1) {
+                return response()->json([
+                    'success' => true,
+                    'message' => "Unauthorized"
+                ]);
+            }
+        return $next($request);
+    }
+}
